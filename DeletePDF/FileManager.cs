dfc0c9b4c40ea96a8136aa0c73f4bfd94796dd84
enginyilmz <enginyilmaz.math@hotmail.com>
2017-08-02 16:42:45 +0300

    .

---
+++ DeletePDF/FileManager.cs
@@ -0,0 +1,92 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+
+namespace DeletePDF
+{
+    public class FileManager
+    {
+        SimpleLogger logger = new SimpleLogger(true);
+        public FileManager()
+        {
+        }
+        public void FindAndDelete()
+        {
+            PathContainer.Prepare();
+            DateTime checkDay = GetCheckDay();
+            foreach (string itemPath in PathContainer.pathContainer)
+            {
+                logger.Info("Market :" + itemPath);
+                string[] fileList = Directory.GetFiles(itemPath).Where(x => new FileInfo(x).CreationTime.Date < checkDay).ToArray();
+                DeleteFile(fileList);
+            }
+        }
+        DateTime GetCheckDay()
+        {
+            int AddDay = Convert.ToInt32(string.IsNullOrEmpty(ConfigurationManager.AppSettings["AddDay"].ToString()) ? "0" : ConfigurationManager.AppSettings["AddDay"].ToString());
+            return DateTime.Now.AddDays(-1 * AddDay);
+        }
+        public void DeleteFile(string[] fileList)
+        {
+            foreach (var item in fileList)
+            {
+                string curFilePath = item;
+                if (File.Exists(curFilePath))
+                {
+                    try
+                    {
+                        logger.Info("Deleted :" + curFilePath);
+                        File.Delete(curFilePath);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(string.Format("Deleted Exception :{0}; \n{1}; \n{2}", curFilePath, ex.Message, ex.InnerException.Message));
+                    }
+                    
+                }
+            }
+        }
+    }
+
+    public class PathContainer
+    {
+        public static List<string> pathContainer = new List<string>();
+        public PathContainer()
+        {
+        }
+
+        public static void Prepare()
+        {
+            pathContainer.Clear();
+
+            PreparePathContainer();
+        }
+
+        private static void AddPath(string path)
+        {
+            pathContainer.Add(path);
+        }
+
+        private static void PreparePathContainer()
+        {
+            string[] PathArray = 
+            { 
+                ConfigurationManager.AppSettings["PathFi"].ToString(), 
+                ConfigurationManager.AppSettings["PathDe"].ToString(), 
+                ConfigurationManager.AppSettings["PathNo"].ToString(), 
+                ConfigurationManager.AppSettings["PathSe"].ToString(), 
+                ConfigurationManager.AppSettings["Test"].ToString() 
+            };
+            foreach (var item in PathArray)
+            {
+                if (Directory.Exists(item))
+                {
+                    AddPath(item);
+                }
+            }
+        }
+    }
+}
---
+++ DeletePDF/Service1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DeletePDF
+{
+    public partial class Service1 : ServiceBase
+    {
+        SimpleLogger logger = new SimpleLogger(true);
+        static Timer _timer;
+        static string _ScheduledRunningTime = ConfigurationManager.AppSettings["ScheduledRunningTime"].ToString();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                logger.Info("OnStart method at :" + DateTime.Now);
+                _timer = new Timer();
+                _timer.Interval = 1 * 60 * 1000;//Every one minute
+                _timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+                _timer.Start();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(string.Format("Start Exception :{0}; \n{1}; \n{2}", ex.ToString(), ex.Message, ex.InnerException.Message));
+            }
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+
+        static void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            string _CurrentTime = String.Format("{0}:{1}", DateTime.Now.Hour, DateTime.Now.Minute);
+            if (_CurrentTime == _ScheduledRunningTime)
+            {
+                (new FileManager()).FindAndDelete();
+            }
+        }
+    }
+}
